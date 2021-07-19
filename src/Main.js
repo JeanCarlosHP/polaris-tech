941ac5d7e20a7ae0a0081a311aaa304eec0a2ac8
Jean Carlos <jeanchpaula@gmail.com>
2021-07-19 19:31:33 -0300

    importação do Verification

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
 ﻿import { Star } from './Star.js'
+import { Verification } from './Verification.js'
 
 const Main = {
   start() {
     Star.createStar()
     Star.checkOrientationAndResize()
+    Verification.checkUserPass()
   }
 }
 
