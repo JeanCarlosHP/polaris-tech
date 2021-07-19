a78837e1f1d3e843dc2a7995926b2d9ee0e486d0
Jean Carlos <jeanchpaula@gmail.com>
2021-07-19 19:31:06 -0300

    verificação básica do input de usuario e senha

---
+++ src/Verification.js
@@ -0,0 +1,20 @@
+﻿const Verification = {
+  checkUserPass() {
+    const buttonLogin = document.querySelector('button#login')
+
+    buttonLogin.addEventListener('click', () => {
+      const user = document.querySelector('#user')
+      const pass = document.querySelector('#pass')
+
+      if (user.value && pass.value === 'admin') {
+        return
+      } else {
+        const invalidCredencial = document.querySelector('.invalid-credencial')
+
+        invalidCredencial.classList.toggle('active')
+      }
+    })
+  }
+}
+
+export { Verification }
